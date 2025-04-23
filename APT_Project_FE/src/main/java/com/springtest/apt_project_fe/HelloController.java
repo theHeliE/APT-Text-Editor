0a2f448afdc737742e4265f24eef45d4d2888469
theHazem <129796088+theHeliE@users.noreply.github.com>
2025-04-23 23:47:48 +0200

    Initial Commit

---
+++ APT_Project_FE/src/main/java/com/springtest/apt_project_fe/HelloController.java
@@ -0,0 +1,14 @@
+package com.springtest.apt_project_fe;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ APT_Project_FE/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.springtest.apt_project_fe {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.springtest.apt_project_fe to javafx.fxml;
+    exports com.springtest.apt_project_fe;
+}
