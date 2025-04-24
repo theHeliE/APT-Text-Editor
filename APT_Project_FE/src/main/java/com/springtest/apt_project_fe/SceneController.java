0655972093b83e16e9c437022173d45b577ce864
theHeliE <129796088+theHeliE@users.noreply.github.com>
2025-04-25 01:37:06 +0300

    Scene Controller

---
+++ APT_Project_FE/src/main/java/com/springtest/apt_project_fe/SceneController.java
@@ -0,0 +1,33 @@
+package com.springtest.apt_project_fe;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SceneController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+
+    public void switchToSceneNew(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("newdoc.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToSceneMain(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
